8d37bc9fd21e73497a96be74577b19647ac30102
Vaidika <136914466+vaidika01@users.noreply.github.com>
2025-01-18 17:42:42 +0530

    Update index.js port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 const db = new pg.Client({
   user: process.env.DB_USER,
